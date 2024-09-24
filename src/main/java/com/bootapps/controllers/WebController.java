ec6101741d0717ca5df5756245d291b64764937d
vinodh1988 <k.c.vinodh@hotmail.com>
2024-09-24 13:35:27 +0530

    controller-integration

--- src/main/java/com/bootapps/controllers/WebController.java
+++ src/main/java/com/bootapps/controllers/WebController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bootapps.services.DataService;
+import com.bootapps.utilities.ApplicationRequestCounter;
+import com.bootapps.utilities.RequestCounter;
 
 @Controller
 @RequestMapping("/webapp")
@@ -14,10 +16,18 @@ public class WebController {
 	
 	@Autowired
 	private DataService dservice;
+	@Autowired
+	RequestCounter sessionscoped;
+	@Autowired
+	ApplicationRequestCounter applicationScoped;
 
 	@GetMapping("/home")
 	public String first(ModelMap map) {
+		sessionscoped.setCounter();
+		applicationScoped.setCounter();
 		map.addAttribute("names",dservice.getNames());
+		map.addAttribute("scount",sessionscoped.getCounter());
+		map.addAttribute("ascoped",applicationScoped.getCounter());
 		return "home";
 	}
 }
--- src/main/java/com/bootapps/utilities/ApplicationRequestCounter.java
+++ src/main/java/com/bootapps/utilities/ApplicationRequestCounter.java
@@ -1,5 +1,23 @@
 package com.bootapps.utilities;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value="application",proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class ApplicationRequestCounter {
+	
+	private Integer Counter=0;
+
+	public Integer getCounter() {
+		return Counter;
+	}
+
+	public void setCounter() {
+		Counter++;
+	}
+	
+	
 
 }
