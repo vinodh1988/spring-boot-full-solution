a6cf6bec3b43b7df8d3d5045266769e42cd33acd
vinodh1988 <k.c.vinodh@hotmail.com>
2024-09-24 13:07:01 +0530

    service

--- src/main/java/com/bootapps/controllers/WebController.java
+++ src/main/java/com/bootapps/controllers/WebController.java
@@ -1,15 +1,23 @@
 package com.bootapps.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bootapps.services.DataService;
 
 @Controller
 @RequestMapping("/webapp")
 public class WebController {
+	
+	@Autowired
+	private DataService dservice;
 
 	@GetMapping("/home")
-	public String first() {
+	public String first(ModelMap map) {
+		map.addAttribute("names",dservice.getNames());
 		return "home";
 	}
 }
--- src/main/java/com/bootapps/services/DataServiceImpl.java
+++ src/main/java/com/bootapps/services/DataServiceImpl.java
@@ -1,5 +1,8 @@
 package com.bootapps.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class DataServiceImpl implements DataService {
 
 	@Override
