1aa07ebe2b1168e41e2f758147fedc01d04477dd
vinodh1988 <k.c.vinodh@hotmail.com>
2024-09-25 11:32:33 +0530

    fullflow

--- src/main/java/com/bootapps/services/ProductService.java
+++ src/main/java/com/bootapps/services/ProductService.java
@@ -1,5 +1,21 @@
 package com.bootapps.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bootapps.entitities.Product;
+import com.bootapps.repos.ProductRepository;
+
+@Service
 public class ProductService {
 
+	@Autowired
+	private ProductRepository prepo;
+	// data jpa will create an implementation for Product Repository and inject
+	
+	public List<Product> getProducts() {
+		return prepo.findAll();
+	}
 }
