851b1fefe5f7347402511c785c73f99d0f341d6b
vinodh1988 <k.c.vinodh@hotmail.com>
2024-09-25 12:38:37 +0530

    repochange

--- src/main/java/com/bootapps/repos/ProductRepository.java
+++ src/main/java/com/bootapps/repos/ProductRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.bootapps.entitities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer>{
-     public Product findByProductNo(Integer productno);
+     public Product findByProductno(Integer productno);
 }
