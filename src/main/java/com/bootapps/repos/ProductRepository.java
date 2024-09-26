b28626ac6d8a6b01347761f81e011f4e887508d0
vinodh1988 <k.c.vinodh@hotmail.com>
2024-09-26 10:10:40 +0530

    custom-query

--- src/main/java/com/bootapps/repos/ProductRepository.java
+++ src/main/java/com/bootapps/repos/ProductRepository.java
@@ -1,9 +1,17 @@
 package com.bootapps.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.bootapps.entitities.Product;
 
+
 public interface ProductRepository extends JpaRepository<Product, Integer>{
      public Product findByProductno(Integer productno);
+   
+     @Query("SELECT p FROM Product p where p.price between :min and :max")
+     public List<Product> findByPriceRange(@Param("min")int min, @Param("max")int max);
 }
