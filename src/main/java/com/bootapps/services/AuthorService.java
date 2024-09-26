bbbc785299e33ad606a7a76d12ad9fa4cde3fa98
vinodh1988 <k.c.vinodh@hotmail.com>
2024-09-26 11:40:22 +0530

    author-books

--- src/main/java/com/bootapps/services/AuthorService.java
+++ src/main/java/com/bootapps/services/AuthorService.java
@@ -1,8 +1,11 @@
 package com.bootapps.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bootapps.entitities.Author;
 import com.bootapps.repos.AuthorRepository;
 
 @Service
