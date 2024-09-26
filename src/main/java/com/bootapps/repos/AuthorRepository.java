0caf9de13faaf82cef5f7fde1ee4b26814701c6d
vinodh1988 <k.c.vinodh@hotmail.com>
2024-09-26 11:26:47 +0530

    author-books

---
+++ src/main/java/com/bootapps/repos/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.bootapps.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bootapps.entitities.Author;
+
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
+
+}
